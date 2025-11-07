42a85ab0a68ed1e6dc20ceb500a73b94103f0b24
christal <christalynn_lipscomb@venkel.com>
2025-11-07 13:55:15 -0600

    fixed errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "js/**/*.js",
+      "public/js/**/*.js",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
